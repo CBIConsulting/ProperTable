f7b38620836ca57ef535546c269ce97f7fd8f2a1
algm <algm85@gmail.com>
2015-08-12 14:07:46 +0200

    Project setup

---
+++ tests.webpack.js
@@ -0,0 +1,6 @@
+// ES5 shims for Function.prototype.bind, Object.prototype.keys, etc.
+require('core-js/es5');
+// Replace ./src/js with the directory of your application code and
+// make sure the file name regexp matches your test files.
+var context = require.context('./src/js', true, /-test\.js$/);
+context.keys().forEach(context);
