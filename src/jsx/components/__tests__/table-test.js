f08a95d04260f33ec6d7b4fa9bf22e614a3380af
algm <algm85@gmail.com>
2016-03-18 12:36:09 +0100

    fixing tests

--- src/jsx/components/__tests__/table-test.js
+++ src/jsx/components/__tests__/table-test.js
@@ -14,7 +14,7 @@ describe('ProperTable', () => {
 				cols={[{name: 'col1', label: 'col1', field: 'id', formatter: (value) => {
 					executed = true;
 
-					return 'hola';
+					return value;
 				}}]}
 				data={[{
 					id: 1
@@ -26,6 +26,39 @@ describe('ProperTable', () => {
 	});
 
 	describe('selection', () => {
+		describe('single selection', () => {
+			let testProps = {}, component;
 
+			beforeEach(() => {
+				let cols = [{
+					name: 'col1',
+					label: 'col1',
+					field: 'id',
+					formatter: (value) => {
+						console.log(value);
+						return <span id={"id_"+value} className={"id_"+value}>{value}</span>;
+					}
+				}];
+				let data = [];
+
+				for (let i = 0; i < 10; i++) {
+					data.push({
+						id: i + 1
+					});
+				};
+
+				testProps = {
+					cols: cols,
+					data: data
+				};
+
+				component = TestUtils.renderIntoDocument(<ProperTable {...testProps} />)
+			});
+
+			it('selects a single row', () => {
+			//	let node = TestUtils.findRenderedDOMComponentWithClass(component, 'id_3');
+				console.log(component, document.getElementById('id_1'));
+			});
+		});
 	});
 });
